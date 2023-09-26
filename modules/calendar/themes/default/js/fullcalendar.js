d96f235c4896cbdbb36e08d3cda9a712f2923750
Nicolás <nicolas@issabel.com>
2023-09-26 08:00:45 -0400

    Set contrasting text color based on chosen event background to avoid white text on white background

--- modules/calendar/themes/default/js/fullcalendar.js
+++ modules/calendar/themes/default/js/fullcalendar.js
@@ -1879,6 +1879,32 @@ function setDayID(cell, date) {
 	});
 }
 
+function getContrast (hexcolor){
+
+	// If a leading # is provided, remove it
+	if (hexcolor.slice(0, 1) === '#') {
+		hexcolor = hexcolor.slice(1);
+	}
+
+	// If a three-character hexcode, make six-character
+	if (hexcolor.length === 3) {
+		hexcolor = hexcolor.split('').map(function (hex) {
+			return hex + hex;
+		}).join('');
+	}
+
+	// Convert to RGB value
+	let r = parseInt(hexcolor.substr(0,2),16);
+	let g = parseInt(hexcolor.substr(2,2),16);
+	let b = parseInt(hexcolor.substr(4,2),16);
+
+	// Get YIQ ratio
+	let yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+
+	// Check contrast
+	return (yiq >= 128) ? 'black' : 'white';
+
+}
 
 function getSkinCss(event, opt) {
 	var source = event.source || {};
@@ -1904,14 +1930,16 @@ function getSkinCss(event, opt) {
 		source.textColor ||
 		opt('eventTextColor');
 	var statements = [];
+
 	if (backgroundColor) {
 		statements.push('background-color:' + backgroundColor);
+        statements.push('color:' + getContrast(backgroundColor));
 	}
 	if (borderColor) {
 		statements.push('border-color:' + borderColor);
 	}
 	if (textColor) {
-		statements.push('color:' + textColor);
+	//	statements.push('color:' + textColor);
 	}
 	return statements.join(';');
 }
